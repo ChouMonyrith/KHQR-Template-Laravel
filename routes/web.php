678212831b493298223b604a4ca56955d2fbff47
ChouMonyrith <152832364+ChouMonyrith@users.noreply.github.com>
2025-10-28 18:31:33 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\Api\PaymentWebhookController;
 // Route::get('/orders/{order}/pay', [PaymentController::class, 'showQR'])->name('orders.pay');
 // Route::get('/orders/{order}/check-status', [PaymentController::class, 'checkStatus'])->name('orders.check');
 
+Route::get('/', function () {
+    return redirect(route('khqr.pay'));
+});
 
 Route::get('/khqr/pay', [PaymentController::class, 'manualPay'])->name('khqr.pay');
 Route::post('/khqr/check', [PaymentController::class, 'checkStatus'])->name('khqr.check');
